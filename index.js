ac140c5051dc30f71a54ca89d504ed6ce7d785b9
unknown <awesomeriptide@gmail.com>
2018-10-21 13:13:41 +0800

    changed port

--- index.js
+++ index.js
@@ -91,5 +91,5 @@ app.use('/graphql', graphqlHTTP({
     rootValue: root,
     graphiql: true,
 }));
-app.listen(4000);
+app.listen(443);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
